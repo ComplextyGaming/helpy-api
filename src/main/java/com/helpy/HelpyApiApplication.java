3a744ddb608812ec599a04562a858d1c594082a5
DiegoJohnsonL <u201714835@upc.edu.pe>
2021-09-07 13:16:47 -0500

    Update pom.xml
    Added dependencies

---
+++ src/main/java/com/helpy/HelpyApiApplication.java
@@ -0,0 +1,13 @@
+package com.helpy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/helpy/HelpyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
